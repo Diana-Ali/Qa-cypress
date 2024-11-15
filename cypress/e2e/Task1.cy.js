b2aeee90f442c6e111a8e47bd33cc6dcd317c5f6
Diana <1183192@student.birzeit.edu>
2024-11-15 21:09:48 +0100

    First commit

---
+++ cypress/e2e/Task1.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress"/>
+
+
+describe("Selecto Task" ,() =>{
+
+it(" Selector" ,()=> {
+    cy.visit("https://demo.productionready.io/#/register")
+
+cy.get("a.navbar-brand ")//conduit link
+cy.get(".nav-link").contains("Home")//home link
+
+cy.get(".navbar.navbar-light").find("li").contains("Sign up")//sign up link
+//cy.contains(".col-md-6 h1","Sign up")
+cy.get("[ng-bind='$ctrl.title']").first()//get sign up title
+ cy.get(".text-xs-center").contains("a","Have an account?")// get link
+ cy.get("fieldset").find("input").filter("[placeholder='Username']").type("Diana")//get username
+ cy.get("[type='email']").type("dsfi9475@gmail.com")//get email
+ cy.get(".ng-pristine.form-control").eq(0).type("1234diana-")//get pass
+ cy.get("[type='submit']").click()//get sign up
+cy.contains(".logo-font","conduit")//get conduit
+cy.contains("span","© 2024. An interactive learning project from")//get footer
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+})
